9a25325b05948be7a679de3550534ed8f3cdfa48
VitalinaKorchova <vitalina.korchova@gmail.com>
2026-03-05 20:00:13 +0200

    Updates

--- _next/static/gm7H9Q4Aege06gdLjKLFy/_buildManifest.js
+++ _next/static/gm7H9Q4Aege06gdLjKLFy/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/CurrencyConverter//_next/:path+",
+        "destination": "/CurrencyConverter/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
